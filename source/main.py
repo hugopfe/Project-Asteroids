fc3574d272fe78650b65f53f247c363155f5118e
Hugo Fernandes <huguinho.p.fernandes@gmail.com>
2021-06-24 10:24:05 -0300

    Create main.py

---
+++ source/main.py
@@ -0,0 +1,19 @@
+import menus
+import pygame
+from util import decode_b64_img
+from images import asteroid
+from game import *
+
+# This module will call the Main Menu and continues on menus module
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('Asteroids')
+
+    icon = pygame.image.load(decode_b64_img(asteroid))
+    pygame.display.set_icon(icon)
+
+    # menus.MainMenu(Game)
+    Game()
+
+    pygame.quit()
